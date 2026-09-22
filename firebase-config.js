e910c817d771259212b8b08cc5ee64bfdccf76c2
daisukekakinuma <kakidai6514@gmail.com>
2026-09-23 07:40:00 +0900

    Add household app with private Firebase sync

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCUxIamZ1V-HBoJPRP-xMGI1jTaxMiWupg',
+  authDomain: 'daily-agents-home.firebaseapp.com',
+  projectId: 'daily-agents-home',
+  storageBucket: 'daily-agents-home.firebasestorage.app',
+  messagingSenderId: '551861656694',
+  appId: '1:551861656694:web:7323b2abc3227c8498d357'
+};
